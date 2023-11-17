1f7ef49ac2750e53339f2fced005247ec14b06c7
Soumitra <projwebinfo@gmail.com>
2023-11-17 20:48:22 +0530

    Create TeacherDao.java

---
+++ spring-mvc-jdbc-crud/spring-thymeleaf/src/main/java/com/roytuts/springmvc/jdbc/crud/dao/TeacherDao.java
@@ -0,0 +1,44 @@
+package com.roytuts.springmvc.jdbc.crud.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.roytuts.springmvc.jdbc.crud.model.Teacher;
+import com.roytuts.springmvc.jdbc.crud.row.mapper.TeacherRowMapper;
+
+@Repository
+@Transactional
+public class TeacherDao {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	public Teacher getTeacher(final int id) {
+		Teacher teacher = jdbcTemplate.queryForObject("select * from teacher where id = ?", new TeacherRowMapper(), id);
+		return teacher;
+	}
+
+	public List<Teacher> getTeachers() {
+		List<Teacher> teachers = jdbcTemplate.query("select * from teacher", new TeacherRowMapper());
+		return teachers;
+	}
+
+	public void addTeacher(final Teacher teacher) {
+		jdbcTemplate.update("insert into teacher(name, expertise) values(?, ?)",
+				new Object[] { teacher.getName(), teacher.getExpertise() });
+	}
+
+	public void updateTeacher(final Teacher teacher) {
+		jdbcTemplate.update("update teacher set name = ?, expertise = ? where id = ?",
+				new Object[] { teacher.getName(), teacher.getExpertise(), teacher.getId() });
+	}
+
+	public void deleteTeacher(final int id) {
+		jdbcTemplate.update("delete from teacher where id = ?", new Object[] { id });
+	}
+
+}
