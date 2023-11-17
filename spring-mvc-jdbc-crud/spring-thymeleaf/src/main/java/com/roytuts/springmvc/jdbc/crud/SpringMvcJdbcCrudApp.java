22925b1baad4889da331cf459e6e85b3532b2535
Soumitra <projwebinfo@gmail.com>
2023-11-17 20:47:13 +0530

    Create SpringMvcJdbcCrudApp.java

---
+++ spring-mvc-jdbc-crud/spring-thymeleaf/src/main/java/com/roytuts/springmvc/jdbc/crud/SpringMvcJdbcCrudApp.java
@@ -0,0 +1,13 @@
+package com.roytuts.springmvc.jdbc.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcJdbcCrudApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcJdbcCrudApp.class, args);
+	}
+
+}
